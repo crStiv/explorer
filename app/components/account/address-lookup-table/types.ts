cff5b46eb90419c6da5d130703f9565a2be1bba0
crStiv <cryptostiv7@gmail.com>
2024-11-24 21:55:21 +0100

    typos types.ts
    in "AdressLookupTable" there was a "1" in "Table" instead of "l"

--- app/components/account/address-lookup-table/types.ts
+++ app/components/account/address-lookup-table/types.ts
@@ -2,7 +2,7 @@ import { Address } from 'web3js-experimental';
 
 const LOOKUP_TABLE_ACCOUNT_TYPE = 1;
 const PROGRAM_ID =
-    'AddressLookupTab1e1111111111111111111111111' as Address<'AddressLookupTab1e1111111111111111111111111'>;
+    'AddressLookupTable1111111111111111111111111' as Address<'AddressLookupTable1111111111111111111111111'>;
 
 export function isAddressLookupTableAccount(accountOwner: Address, accountData: Uint8Array): boolean {
     if (accountOwner !== PROGRAM_ID) return false;
